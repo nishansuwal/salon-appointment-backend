e4993456271b5984279e0e1b78e2bdb417e45e46
nishan <nishansuwal2020@gmail.com>
2026-08-19 20:31:22 +0545

    feat: Update CORS configuration to allow additional frontend URL

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173', env('APP_FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
